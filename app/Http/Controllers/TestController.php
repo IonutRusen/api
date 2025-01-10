e811c39d38c04cb3993c1a589b9b045a77f9e004
Ionut Rusen <ionut@splashcreative.com>
2025-01-10 13:14:06 +0200

    Add category processing and database seeding logic
    This commit introduces logic to process categories from a JSON file and seed them into the database. Parent categories are identified and linked to their respective child categories using the parent_aliases key. Chunking is implemented to handle large datasets efficiently during seeding.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,26 +5,69 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Resources\v1\CompanyResource;
+use App\Models\Categories;
 use App\Queries\v1\FetchCompanies;
 use Illuminate\Http\Request;
 
 final class TestController extends Controller
 {
-    public function __construct(
-        private FetchCompanies $query,
-    ) {}
+
     public function __invoke(Request $request)
     {
+        /*{
+      "alias": "specialbikes",
+      "title": "Special Bikes",
+      "parent_aliases": [
+        "bicycles"
+      ],
+      "country_whitelist": [
+        "DK",
+        "PT"
+      ],
+      "country_blacklist": []
+    },
+    {
+      "alias": "specialed",
+      "title": "Special Education",
+      "parent_aliases": [
+        "education"
+      ],
+      "country_whitelist": [],
+      "country_blacklist": [
+        "FI"
+      ]
+    },*/
+        // load categories from categories json file
 
-        return  CompanyResource::collection(
-            resource: $this->query->handle(
-                includes: ['addresses'],
-                sortBy: $request->input('sortBy'),
-                order: $request->input('orderBy'),
-            )->paginate(
-                perPage: $request->input('itemsPerPage') ?? config('app.per_page'),
-                page: $request->input('page'),
-            ),
-        );
+        $categories = collect(collect(json_decode(file_get_contents(base_path('categories.json'))))['categories']);
+
+        $parentCategories = collect($categories->where('parent_aliases', null)->all());
+
+
+        $parentCategories->each(function ($parentCateg) {
+            Categories::create([
+                'alias' => $parentCateg->alias,
+                'title' => $parentCateg->title,
+
+            ]);
+        });
+
+        $categories->chunk(100)->each(function ($categories) {
+            $categories->map(function ($category) {
+                if ($category->parent_aliases) {
+                    $parent = Categories::where('alias', $category->parent_aliases[0])->first();
+                    if ($parent) {
+                        return Categories::create([
+                            'alias' => $category->alias,
+                            'title' => $category->title,
+                            'parent_id' => $parent->id,
+                        ]);
+                    }
+                }
+                
+            });
+        });
+return response()->json(['message' => 'Categories created successfully'], 200);
+
     }
 }
